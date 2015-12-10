37488dc98ceeed3437ebc2d49562562739e456e1
Chazz <chzzs89@gmail.com>
2015-12-10 15:33:34 +0100

    workaround in preshandler

--- contentServer/app/controllers/presHandler.js
+++ contentServer/app/controllers/presHandler.js
@@ -10,6 +10,7 @@ var presHandler = function() {
 	this.jsonStrPres=null;
 	this.jsonArray=null;
 	this.currentSlidNb=0;
+	this.currentSlidId ='';
 	this.status=false;
 };
 
@@ -22,54 +23,51 @@ presHandler.prototype.getPresId=function(){
 };
 
 presHandler.prototype.nextSlid = function() {
-	if((this.jsonarray==null))
-	{
+	if((this.jsonarray==null)) {
 		return false;
 	}
-	if((this.currentSlidNb==this.jsonarray.length-1))
-	{
+	if((this.currentSlidNb==this.jsonarray.length-1))	{
 		return false;
 	}
 
 	this.currentSlidNb=this.currentSlidNb+1;
 	return true;
-	
+
 };
 
 presHandler.prototype.prevSlid = function() {
-	if((this.jsonarray==null))
-	{
+	if((this.jsonarray==null)) {
 		return false;
 	}
-	if((this.currentSlidNb==0))
-	{
+	if((this.currentSlidNb==0))	{
 		return false;
 	}
 
 	this.currentSlidNb=this.currentSlidNb-1;
 	return true;
-	
+
 };
 
-
-
-
 presHandler.prototype.getCurrentSlid = function() {
-
 	return this.jsonarray[this.currentSlidNb];
 };
 
 presHandler.prototype.init=function(currentPresId){
 	this.presId=currentPresId;
 	this.jsonStrPres=fs.readFileSync(CONFIG.presentationDirectory+'/'+currentPresId+".pres.json");
-	this.jsonarray=JSON.parse(this.jsonStrPres).slidArray;
+	var i = 0;
+	this.jsonarray = [];
+
+	//Please do not judge us. It is a temporary workaround.
+	for(var slide in JSON.parse(this.jsonStrPres).slidArray) {
+		this.jsonarray[i++]= JSON.parse(this.jsonStrPres).slidArray[slide];
+	}
 	this.status=true;
-	
-	
+
 };
 
 presHandler.prototype.endPres = function() {//reset the pres obj
-		this.presId=null;
+	this.presId=null;
 	this.jsonStrPres=null;
 	this.jsonArray=null;
 	this.currentSlidNb=0;
@@ -77,4 +75,3 @@ presHandler.prototype.endPres = function() {//reset the pres obj
 };
 
 module.exports=presHandler;
-
